acebc911ebbeec2f976efa04fec5a6f9013420a4
Marie Treschow <treschow.marie@gmail.com>
2016-10-07 13:29:57 +0200

    first colors

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+var colors = require('colors');
+
+console.log('hello'.green);
+console.log('hello'.red);
+console.log('hello'.blue);
+console.log('hello'.yellow);
+console.log('hello'.magenta);
+console.log('hello'.cyan);
+console.log('hello'.grey);
